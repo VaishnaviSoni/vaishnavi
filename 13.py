27f4c4b8200c4fa18638d5206002637aee166853
VaishnaviSoni <44240100+VaishnaviSoni@users.noreply.github.com>
2018-11-30 02:34:17 -0800

    Add files via upload

--- 13.py
+++ 13.py
@@ -1,5 +1,8 @@
 n=input()
-if n== "a" or n=="e" or n=="i" or n=="o" or n=="u":
-    print("Vowel")
+if (n>="a" and n<="z") or (n>="A" and n<="Z"):
+    if n=="A" or n=="E" or n=="I" or n=="O" or n=="U" or n=="a" or n=="e" or n=="i" or n=="o" or n=="u":
+        print("vowel")
+    else:
+        print("consonant")
 else:
-    print("Consonant")
+    print("invalid input")
