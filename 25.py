9a5230dd700e6e6012b03cfd2af7b8f3b7e60646
Vaishnavi Soni <44240100+VaishnaviSoni@users.noreply.github.com>
2019-05-14 03:15:44 -0700

    Add files via upload

--- 25.py
+++ 25.py
@@ -3,11 +3,7 @@ n=int(t[0])
 q=int(t[1])
 if (n<=100000)and(q<=100000):
     for i in range(n+1,q):
-        if(i>1):
-            for j in range(2,i):
-                if(i%j)==0:
-                    break
-        else:
+        if (i%2)==0:
             print(i,end=" ")
 else:
     print("invalid")
