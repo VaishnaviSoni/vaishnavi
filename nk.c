3a8f520929af2d7097f67848b5ce853af8813228
Vaishnavi Soni <44240100+VaishnaviSoni@users.noreply.github.com>
2019-05-14 01:26:41 -0700

    Add files via upload

---
+++ nk.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main(){
+	int i,n,k,a[100],s=0;
+	scanf("%d%d",&n,&k);
+	for(i=0;i<n;i++){
+		scanf("%d",&a[i]);
+			}
+			for(i=0;i<k;i++){
+				s=s+a[i];}
+				
+			printf("%d",s);
+			
+}
